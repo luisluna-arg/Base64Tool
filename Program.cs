3dd913ce7372928d39dfc782a5ecea6f97b65374
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2024-03-13 11:42:07 -0300

    Added argument management

--- Program.cs
+++ Program.cs
@@ -1,27 +1,37 @@
-﻿using System.Text;
+using System.Text;
 
-Console.Write("Enter the Base64 encoded string: ");
-var base64String = Console.ReadLine();
+Func<string, string> decipher = (base64String) => Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
 
-try
+if (args.Length >= 1 && args.Contains("-d") && !string.IsNullOrWhiteSpace(args[1]))
 {
-    if (!string.IsNullOrWhiteSpace(base64String))
-    {
-        var data = Convert.FromBase64String(base64String);
-        var decodedString = Encoding.UTF8.GetString(data);
-        Console.WriteLine();
-        Console.WriteLine("Decoded string\n===============");
-        Console.WriteLine(decodedString);
-        Console.WriteLine();
-        Console.Write("Press any key to close");
-        Console.ReadKey();
-    }
-    else
-    {
-        Console.WriteLine("Cannot decipher null string");
-    }
+    Console.WriteLine();
+    Console.WriteLine("Decoded string\n===============");
+    Console.WriteLine(decipher(args[1]));
 }
-catch (Exception ex)
+else
 {
-    Console.WriteLine("Error: " + ex.Message);
+    try
+    {
+        Console.Write("Enter the Base64 encoded string: ");
+        var base64String = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(base64String))
+        {
+            var decodedString = decipher(base64String);
+            Console.WriteLine();
+            Console.WriteLine("Decoded string\n===============");
+            Console.WriteLine(decodedString);
+            Console.WriteLine();
+            Console.Write("Press any key to close");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("Cannot decipher null string");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
 }
