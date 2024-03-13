238bd27e020fbe82ed2e26aa7cc4f4b82bdac4b3
Luis Luna <33042685+luisluna-arg@users.noreply.github.com>
2024-03-13 11:24:59 -0300

    Init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+Console.Write("Enter the Base64 encoded string: ");
+var base64String = Console.ReadLine();
+
+try
+{
+    if (!string.IsNullOrWhiteSpace(base64String))
+    {
+        var data = Convert.FromBase64String(base64String);
+        var decodedString = Encoding.UTF8.GetString(data);
+        Console.WriteLine();
+        Console.WriteLine("Decoded string\n===============");
+        Console.WriteLine(decodedString);
+        Console.WriteLine();
+        Console.Write("Press any key to close");
+        Console.ReadKey();
+    }
+    else
+    {
+        Console.WriteLine("Cannot decipher null string");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+dotnet publish --output "C:/_Github/_Base64App/publish-folder" --runtime win-x64 --configuration Release -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true
